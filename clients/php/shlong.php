f465923704a5e312f330bfcbe5567158487b3dbe
Michal Migurski <migurski@or.pair.com>
2009-04-14 01:10:36 -0400

    Added comments

--- clients/php/shlong.php
+++ clients/php/shlong.php
@@ -2,6 +2,7 @@
 
     function shlong_get_short_url($shlong_base, $long_url)
     {
+        // first, check whether the requested long URL already has a short URL
         $c = curl_init();
         curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($c, CURLOPT_URL, rtrim($shlong_base, '/').'/?'.urlencode($long_url));
@@ -9,11 +10,12 @@
         $short_url = trim(curl_exec($c));
         curl_close($c);
         
+        // if it doesn't, make one
         if(empty($short_url))
         {
             $c = curl_init();
             curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($c, CURLOPT_URL, 'http://teczno.com/s/');
+            curl_setopt($c, CURLOPT_URL, rtrim($shlong_base, '/').'/');
             curl_setopt($c, CURLOPT_POST, 1);
             curl_setopt($c, CURLOPT_POSTFIELDS, 'url='.urlencode($long_url));
         
